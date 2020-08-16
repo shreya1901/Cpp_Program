c5413a1bdba95794e094296435e8fd0d279da8fe
Shreya pandya <64072146+shreya1901@users.noreply.github.com>
2020-08-16 18:53:06 +0530

    Created primenum_sum.cpp
    This program will check whether a number can be express as sum of two Prime Numbers by using user define function.

---
+++ primenum_sum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int func(int num);
+
+int main() {
+   int num , i;
+   cout << "Enter a Positive Integer : ";
+   cin >> num;
+   for(i = 2; i <= num/2; ++i) {
+      if (func(i)) {
+         if (func(num - i)) {
+            cout <<"\n"<< num << " = " << i << " + " << num-i << endl;
+         }
+      }
+   }
+   return 0;
+}
+
+int func(int num) {
+   int i;
+   int flag = 1;
+   for(i = 2; i <= num/2; ++i) {
+      if(num % i == 0) {
+         flag = 0;
+         break;
+      }
+   }
+   return flag;
+}
